15a881f54a567b5b6a36224f7348f3e1ac615724
Abir Chakraborty <abir.c@247-inc.com>
2017-03-02 14:34:42 +0530

    modified

--- app.py
+++ app.py
@@ -2491,7 +2491,7 @@ def makeWebhookResult(req):
             else:
                 speech = properties[0] + " does not have the facility for " + specific_request
 
-        context_out = {"contextOut": all_data}
+        # context_out = {"contextOut": all_data}
         # if not specific_key:
         #     # context_out = {"contextOut": [{"name":"weather", "lifespan":2, "parameters":{"city":"Rome"}}]}
         #     context_out = all_data
